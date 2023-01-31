589d22271336a9dfaabd672c7f0c63fff1144dd8
jaxinewolfe <wolfejax@si.edu>
2023-01-31 18:08:13 -0500

    NGGI WERK

--- nggi_2022/carbon_accumulation.R
+++ nggi_2022/carbon_accumulation.R
@@ -16,7 +16,12 @@
 # Merge climate zones
 # Which/How many habitats are left?
 
+# Case: Carbon Accumulation data is present in the data release only but we dropped it during curation
+# Revisit data curation and see if there are carbon stock values we can revive
+
 # make sure everything is expressed in Carbon
+# keep in mind - depth integration!!
+
 # if only sed accumulation is present, 
 # derive fraction carbon and resulting C accumulation rate using Craft relationship (1991) - we can specify more complexity later
 
@@ -32,28 +37,35 @@
 # Kauffman total ecosystem carbon stocks
 # https://onlinelibrary.wiley.com/doi/10.1111/gcb.15248
 
+# other dating modeling resource
+# https://www.sciencedirect.com/science/article/abs/pii/S1871101420300558
+
 ## Prepare Workspace ####
 
 library(tidyverse)
 library(readxl)
 
 # source synthesis
-source("shared_resources/refresh_data.R")
+source("resources//refresh_data.R")
 
 # read in reported values
-reported <- read_csv("NGGI_2022/NGGI_2022_reported_values - literature_values.csv")
+reported <- read_csv("nggi_2022/input_data/NGGI_2022_reported_values - literature_values.csv") %>% 
+  mutate(study_id = ifelse(study_id %in% c("Krauss_et_al_2018_Flux", "Krauss_et_al_2018_Holocene"),
+                           "Krauss_et_al_2018", study_id))
 
 # read in climate zone table
-climate_zones <- read_xlsx("nggi_2022/inventorying_bib/Climate zones.xlsx") %>% 
+climate_zones <- read_xlsx("nggi_2022/input_data/Climate zones.xlsx") %>% 
   mutate(Color = tolower(Color),
          Color = recode(Color, "oragen" = "orange"),
          State = recode(State, "Delamare" = "Delaware")) %>% 
   rename(admin_division = State, climate_zone = `Climate zone`, climate_color = Color)
 
-# Prepare Core Synthesis ####
+
+# Prepare Synthesis Data ####
 
 # isolate US cores with dating information and join climate zones
-dated_us_cores <- cores %>% filter(country == "United States") %>% 
+dated_us_cores <- cores %>% 
+  filter(country == "United States") %>% 
   # filter(habitat != "upland") %>% 
   drop_na(dates_qual_code) %>% 
   left_join(climate_zones) %>% 
@@ -71,23 +83,59 @@ dated_us_cores <- cores %>% filter(country == "United States") %>%
 # Breithaupt_et_al_2014
 # McTigue_et_al_2020
 
+# find the studies that don't match between the synthesis and the reported table
+nomatch <- distinct(dated_us_cores, study_id) %>% 
+  anti_join(reported %>% distinct(study_id))
+nomatch$study_id
+
+# No associated article: 
+  # Messerschmidt_and_Kirwan_2020 - CCN release, has interval sedimentation rate (mm yr-1). Need to derive %OC and calculate CAR
+  # O'keefe Suttles, other release - CAR needs to be extracted from data releases
+  # Weston_et_al_2020, CCN release - no accumulation rates, we have to calculate these or ask Nat
+  # Buffington_et_al_2020 - CCN release, 1 core annual accretion rate given as 4.8mm/yr, Need to derive %OC since %C is modeled total?
+  # Gonneea_et_al_2018 - other release, interval CAR in original 
+  # Breithaupt_et_al_2020 - CCN release, maybe a rate column? might have to age depth model it
+
+# Need to be added: 
+  # Rodriguez_et_al_2022: https://doi.org/10.1038/s43247-022-00501-x
+  # Vaughn_et_al_2020: https://doi.org/10.1029/2019GB006334
+  # Piazza_et_al_2020 (replace 2011), article is downloaded
+  # Carlin_et_al_2021 (same as Arias-Ortiz pub): https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2021JG006573
+  # Giblin_and_Forbrich_2018: https://agupubs.onlinelibrary.wiley.com/doi/full/10.1002/2017JG004336
+
+
+nggi_bibs <- bib %>% 
+  filter(study_id %in% unique(dated_us_cores$study_id)) %>% 
+  filter(bibtype != "Misc")
+
 gapfill_ds <- depthseries %>% 
   # filter(study_id %in% unique(dated_us_cores$study_id))
   filter(core_id %in% unique(dated_us_cores$core_id)) %>% 
   left_join(dated_us_cores %>% distinct(core_id, habitat)) %>% # add habitat info from cores table
-  left_join(methods %>% distinct(study_id, method_id, fraction_carbon_type)) %>% 
+  left_join(methods %>% distinct(study_id, method_id, fraction_carbon_type)) %>%   
+  select_if(function(x) {!all(is.na(x))}) %>% 
+  # flag rows by the presence/absence of things 
+  mutate(action_flag = case_when(!is.na(fraction_carbon) & fraction_carbon_type == "organic carbon" ~ "no action",
+                                 !is.na(fraction_carbon) & fraction_carbon_type == "total carbon" ~ "convert TC to OC",
+                                 is.na(fraction_carbon) & !is.na(fraction_organic_matter) ~ "convert LOI to C",
+                                 T ~ NA_character_)) %>% 
   # gapfill fraction carbon 
   # relationship to convert total carbon to organic?
   # start with Craft 1991 relationship, increase complexity from there
-  mutate(measured_or_modeled = case_when(is.na(fraction_carbon) ~ "modeled",
-                                         study_id %in% c("Buffington_et_al_2020", "Drexler_et_al_2009", "Keshta_et_al_2020", "Radabaugh_et_al_2018", "Rodriguez_et_al_2022") ~ "modeled",
-                                         T ~ "measured"),
-         soc = case_when(is.na(fraction_carbon) & !is.na(fraction_organic_matter) & habitat == "marsh" ~ 0.4 * (100*fraction_organic_matter) + 0.0025*(100*fraction_organic_matter)^2,
-                         is.na(fraction_carbon) & !is.na(fraction_organic_matter) & habitat == "mangrove" ~ 0.415 * (100*fraction_organic_matter) + 2.89,
-                         fraction_carbon_type == "total carbon" ~ NA_real_,
-                         T ~ fraction_carbon * 100)) %>% 
-  select_if(function(x) {!all(is.na(x))}) %>%
-  select(fraction_organic_matter, contains("carbon"), soc, everything())
+  mutate(soc = case_when(action_flag == "no action" ~ fraction_carbon * 100,
+                         action_flag == "convert LOI to C" & habitat == "marsh" ~ 0.4 * (100*fraction_organic_matter) + 0.0025*(100*fraction_organic_matter)^2,
+                         action_flag == "convert LOI to C" & habitat == "mangrove" ~ 0.415 * (100*fraction_organic_matter) + 2.89,
+                         action_flag == "convert TC to OC" ~ NA_real_,
+                         T ~ NA_real_)
+         # measured_or_modeled = case_when(study_id %in% c("Buffington_et_al_2020", "Drexler_et_al_2009",
+         #                                                     "Keshta_et_al_2020", "Radabaugh_et_al_2018",
+         #                                                     "Rodriguez_et_al_2022") ~ "modeled",
+         #                                 action_flag == "convert LOI to C" ~ "modeled",
+         #                                     T ~ "measured"),
+         ) %>%
+  drop_na(action_flag) %>% 
+  select(study_id, fraction_organic_matter, contains("carbon"), action_flag, soc, everything())
+
 # prepare reported values table and merge with core table
 # do this after?
 # join_tables <- reported %>%
@@ -108,9 +156,9 @@ gapfill_ds %>%
   # filter(study_id == "Drexler_et_al_2009") %>% 
   # filter(habitat == "swamp") %>% 
   drop_na(soc) %>% 
-  ggplot(aes(fraction_organic_matter * 100, soc, col = habitat)) +
+  ggplot(aes(fraction_organic_matter * 100, soc, col = action_flag)) +
   geom_point(alpha = 0.5, pch = 1) +
-  facet_wrap(~measured_or_modeled)
+  facet_wrap(~action_flag)
 
 gapfill_ds %>% drop_na(soc) %>% 
   ggplot(aes(soc)) +
@@ -119,7 +167,9 @@ gapfill_ds %>% drop_na(soc) %>%
 ## Reported Values ####
 
 # investigate table
-accumulation_types <- distinct(reported, study_id, accumulation_type)
+lookup <- distinct(reported, study_id, accumulation_type) %>% 
+  full_join(gapfill_ds %>% distinct(study_id, action_flag)) %>% 
+  arrange(study_id)
 
 ## Pb-210
 
